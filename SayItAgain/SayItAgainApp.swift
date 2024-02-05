833090922f1100b856ca321152f321a23db8e373
Stewart French <stewart.french@gmail.com>
2024-02-05 15:50:03 -0600

    Initial Commit

---
+++ SayItAgain/SayItAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayItAgainApp.swift
+//  SayItAgain
+//
+//  Created by Stewart French on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SayItAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
